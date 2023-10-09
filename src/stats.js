cdae35ca6885ad67881b09385d6e803eeee79b7b
wojake <vpn2843@protonmail.com>
2023-10-09 13:29:02 +0000

    Update stats.js

--- src/stats.js
+++ src/stats.js
@@ -4,7 +4,7 @@ const prompt = require("prompt-sync")({ sigint: true });
 async function main() {
     console.log(`${__filename}: Get Burn2Mint stats from your own local indexer!`);
 
-    console.log(`Data points:\n1. All Account Records.\n2. Unique Account Record.\n3. All History Records.\n4. Daily B2M Amount.\n5. Monthly B2M Amount.\n6. Overall B2M Amount.\n7. Top B2M Records.\n8. EXIT.`);
+    console.log(`Data points:\n1. All Account Records.\n2. Unique Account Record.\n3. Overall B2M Statistics.\n4. All History Records.\n5. Daily B2M Amount.\n6. Monthly B2M Amount.\n7. EXIT.`);
 
     const requestNumber = parseInt(prompt("> Request Number: "));
 
@@ -13,43 +13,53 @@ async function main() {
 
     switch (requestNumber) {
         case 1:
+            // All Account Records
             var data_AllAccountRecord = await dbManager.GetAllAccountRecord();
             console.log(data_AllAccountRecord);
             break;
         case 2:
+            // Unique Account Record
             const accountAddress = prompt(">> Account Address: ")
             var data_AccountRecord = await dbManager.GetAccountRecord(accountAddress);
             console.log(data_AccountRecord);
             break;
         case 3:
+            // Overall B2M statistics
+            var burnt_amount = 0;
+            var minted_amount = 0;
+            var total_funded_accounts = 0;
+            var data_MonthlyHistoryRecord = await dbManager.GetAllHistoryRecord("monthly");
+            data_MonthlyHistoryRecord.forEach(record => {
+                burnt_amount += record.burnt_amount;
+                minted_amount += record.minted_amount;
+                total_funded_accounts += record.newly_funded_account;
+            });
+            console.log(` -- B2M-Funded Account : ${total_funded_accounts} Xahau Accounts`);
+            console.log(` --          Burnt XRP : ${burnt_amount / 1000000} $XRP`);
+            console.log(` --         Minted XAH : ${minted_amount / 1000000} $XAH`);
+            console.log(` --      Un-minted XAH : ${(burnt_amount - minted_amount) / 1000000} $XAH`);
+            break;
+        case 4:
+            // All History Records
             const tableNameAll = prompt(">> Table name (daily/monthly): ");
             var data_AllHistoryRecord = await dbManager.GetAllHistoryRecord(tableNameAll);
             console.log(data_AllHistoryRecord);
             break;
-        case 4:
+        case 5:
+            // Daily B2M amount
             const dailyDate = prompt(">> Date (YYY-MM-DD): ");
             var data_DailyHistoryRecord = await dbManager.GetHistoryRecord("daily", dailyDate);
             console.log(data_DailyHistoryRecord);
             break;
-        case 5:
+        case 6:
+            // Monthly B2M amount
             const monthlyDate = prompt(">> Date (YYYY-MM): ");
             console.log(monthlyDate+"-00");
             var data_MonthlyHistoryRecord = await dbManager.GetHistoryRecord("monthly", monthlyDate+"-00");
             console.log(data_MonthlyHistoryRecord);
             break;
-        case 6:
-            var burnt_amount = 0;
-            var minted_amount = 0;
-            var data_MonthlyHistoryRecord = await dbManager.GetAllHistoryRecord("monthly");
-            data_MonthlyHistoryRecord.forEach(record => {
-                burnt_amount += record.burnt_amount;
-                minted_amount += record.minted_amount;
-            });
-            console.log(` --         Burnt XRP : ${burnt_amount / 1000000} $XRP`);
-            console.log(` --        Minted XAH : ${minted_amount / 1000000} $XAH`);
-            console.log(` --     Un-minted XAH : ${(burnt_amount - minted_amount) / 1000000} $XAH`);
-            break;
-        case 8:
+        case 7:
+            // exit
             break;
         }
 }
